1db48bf3fcb16c87060832ef0c07549b23cdb448
breilingmg <breilingmg@g.cofc.edu>
2026-04-07 19:37:47 -0400

    project 2

---
+++ Run_ServiceQueue_sensitivity_xxx.m
@@ -0,0 +1,220 @@
+Run samples of the ServiceQueue simulation department store Madeline Breiling
+Collect statistics and plot histograms along the way.
+
+Set up
+We'll measure time in minutes
+Arrival rate: 1 every 75 seconds = 0.8 every minute
+lambda = 0.8;
+Departure (service) rate: 1 per 6.5 minutes = 0.15 every minute
+mu = 0.15;
+Number of serving stations
+s = 8;
+Run 100 samples of the queue.
+NumSamples = 100;
+Each sample is run up to a maximum time.
+4 hours = 240 minutes
+MaxTime = 240;
+Make a log entry every so often
+%LogInterval = 1/60;
+%log entry every 5 minutes
+LogInterval = 5;
+
+
+Numbers from theory for M/M/1 queue
+Compute P(1+n) =  = probability of finding the system in state  in the long term. Note that this calculation assumes .
+rho = lambda / mu;
+P0 = 1 - rho;
+nMax = 10;
+P = zeros([1, nMax+1]);
+P(1) = P0;
+for n = 1:nMax
+    P(1+n) = P0 * rho^n;
+end
+
+Run simulation samples
+This is the most time consuming calculation in the script, so let's put it in its own section.  That way, we can run it once, and more easily run the faster calculations multiple times as we add features to this script.
+Reset the random number generator.  This causes MATLAB to use the same sequence of pseudo-random numbers each time you run the script, which means the results come out exactly the same.  This is a good idea for testing purposes.  Under other circumstances, you probably want the random numbers to be truly unpredictable and you wouldn't do this.
+rng("default");
+We'll store our queue simulation objects in this list.
+QSamples = cell([NumSamples, 1]);
+The statistics come out weird if the log interval is too short, because the log entries are not independent enough.  So the log interval should be long enough for several arrival and departure events happen.
+for SampleNum = 1:NumSamples
+    fprintf("Working on sample %d\n", SampleNum);
+    q = ServiceQueue( ...
+        ArrivalRate=lambda, ...
+        DepartureRate=mu, ...
+        NumServers=s, ...
+        LogInterval=LogInterval);
+    q.schedule_event(Arrival(random(q.InterArrivalDist), Customer(1)));
+    run_until(q, MaxTime);
+    QSamples{SampleNum} = q;
+end
+
+Collect measurements of how many customers are in the system
+Count how many customers are in the system at each log entry for each sample run.  There are two ways to do this.  You only have to do one of them.
+Option one: Use a for loop.
+NumInSystemSamples = cell([NumSamples, 1]);
+for SampleNum = 1:NumSamples
+    q = QSamples{SampleNum};
+    % Pull out samples of the number of customers in the queue system. Each
+    % sample run of the queue results in a column of samples of customer
+    % counts, because tables like q.Log allow easy extraction of whole
+    % columns like this.
+    NumInSystemSamples{SampleNum} = q.Log.NumWaiting + q.Log.NumInService;
+end
+Option two: Map a function over the cell array of ServiceQueue objects.
+The @(q) ... expression is shorthand for a function that takes a ServiceQueue as input, names it q, and computes the sum of two columns from its log.  The cellfun function applies that function to each item in QSamples. The option UniformOutput=false tells cellfun to produce a cell array rather than a numerical array.
+NumInSystemSamples = cellfun( ...
+    @(q) q.Log.NumWaiting + q.Log.NumInService, ...
+    QSamples, ...
+    UniformOutput=false);
+Join numbers from all sample runs.
+vertcat is short for "vertical concatenate", meaning it joins a bunch of arrays vertically, which in this case results in one tall column.
+NumInSystem = vertcat(NumInSystemSamples{:});
+MATLAB-ism: When you pull multiple items from a cell array, the result is a "comma-separated list" rather than some kind of array.  Thus, the above means
+NumInSystem = vertcat(NumInSystemSamples{1}, NumInSystemSamples{2}, ...)
+which concatenates all the columns of numbers in NumInSystemSamples into one long column.
+This is roughly equivalent to "splatting" in Python, which looks like f(*args).
+
+Pictures and stats for number of customers in system
+Print out mean number of customers in the system.
+meanNumInSystem = mean(NumInSystem);
+fprintf("Mean number in system: %f\n", meanNumInSystem);
+Make a figure with one set of axes.
+fig = figure();
+t = tiledlayout(fig,1,1);
+ax = nexttile(t);
+MATLAB-ism: Once you've created a picture, you can use hold to cause further plotting functions to work with the same picture rather than create a new one.
+hold(ax, "on");
+Start with a histogram.  The result is an empirical PDF, that is, the area of the bar at horizontal index n is proportional to the fraction of samples for which there were n customers in the system.  The data for this histogram is counts of customers, which must all be whole numbers.  The option BinMethod="integers" means to use bins  so that the height of the first bar is proportional to the count of 0s in the data, the height of the second bar is proportional to the count of 1s, etc. MATLAB can choose bins automatically, but since we know the data consists of whole numbers, it makes sense to specify this option so we get consistent results.
+h = histogram(ax, NumInSystem, Normalization="probability", BinMethod="integers");
+Plot .  If all goes well, these dots should land close to the tops of the bars of the histogram.
+plot(ax, 0:nMax, P, 'o', MarkerEdgeColor='k', MarkerFaceColor='r');
+Add titles and labels and such.
+title(ax, "Number of customers in the system");
+xlabel(ax, "Count");
+ylabel(ax, "Probability");
+legend(ax, "simulation", "theory");
+Set ranges on the axes. MATLAB's plotting functions do this automatically, but when you need to compare two sets of data, it's a good idea to use the same ranges on the two pictures.  To start, you can let MATLAB choose the ranges automatically, and just know that it might choose very different ranges for different sets of data.  Once you're certain the picture content is correct, choose an x range and a y range that gives good results for all sets of data.  The final choice of ranges is a matter of some trial and error.  You generally have to do these commands after calling plot and histogram.
+This sets the vertical axis to go from  to .
+ylim(ax, [0, 0.3]);
+This sets the horizontal axis to go from  to .  The histogram will use bins  so this leaves some visual breathing room on the left.
+xlim(ax, [-1, 21]);
+MATLAB-ism: You have to wait a couple of seconds for those settings to take effect or exportgraphics will screw up the margins.
+pause(2);
+Save the picture as a PDF file.
+exportgraphics(fig, "Number in system histogram.pdf");
+
+Collect measurements of how long customers spend in the system
+This is a rather different calculation because instead of looking at log entries for each sample ServiceQueue, we'll look at the list of served  customers in each sample ServiceQueue.
+Option one: Use a for loop.
+TimeInSystemSamples = cell([NumSamples, 1]);
+for SampleNum = 1:NumSamples
+    q = QSamples{SampleNum};
+    % The next command has many parts.
+    %
+    % q.Served is a row vector of all customers served in this particular
+    % sample.
+    % The ' on q.Served' transposes it to a column.
+    %
+    % The @(c) ... expression below says given a customer c, compute its
+    % departure time minus its arrival time, which is how long c spent in
+    % the system.
+    %
+    % cellfun(@(c) ..., q.Served') means to compute the time each customer
+    % in q.Served spent in the system, and build a column vector of the
+    % results.
+    %
+    % The column vector is stored in TimeInSystemSamples{SampleNum}.
+    TimeInSystemSamples{SampleNum} = ...
+        cellfun(@(c) c.DepartureTime - c.ArrivalTime, q.Served');
+end
+
+Option two: Use cellfun twice.
+The outer call to cellfun means do something to each ServiceQueue object in QSamples.  The "something" it does is to look at each customer in the ServiceQueue object's list q.Served and compute the time it spent in the system.
+TimeInSystemSamples = cellfun( ...
+    @(q) cellfun(@(c) c.DepartureTime - c.ArrivalTime, q.Served'), ...
+    QSamples, ...
+    UniformOutput=false);
+Join them all into one big column.
+TimeInSystem = vertcat(TimeInSystemSamples{:});
+
+Pictures and stats for time customers spend in the system
+Print out mean time spent in the system.
+meanTimeInSystem = mean(TimeInSystem);
+fprintf("Mean time in system: %f\n", meanTimeInSystem);
+Make a figure with one set of axes.
+fig = figure();
+t = tiledlayout(fig,1,1);
+ax = nexttile(t);
+This time, the data is a list of real numbers, not integers.  The option BinWidth=... means to use bins of a particular width, and choose the left-most and right-most edges automatically.  Instead, you could specify the left-most and right-most edges explicitly.  For instance, using BinEdges=0:0.5:60 means to use bins 
+h = histogram(ax, TimeInSystem, Normalization="probability", BinWidth=5/60);
+Add titles and labels and such.
+title(ax, "Time in the system");
+xlabel(ax, "Time");
+ylabel(ax, "Probability");
+Set ranges on the axes.
+ylim(ax, [0, 0.2]);
+xlim(ax, [0, 2.0]);
+Wait for MATLAB to catch up.
+pause(2);
+Save the picture as a PDF file.
+exportgraphics(fig, "Time in system histogram.pdf");
+
+
+1)
+L: expected total number of customers in the system
+L = = =5
+: expected number of customers waiting (not being served)
+
+W: expected total time in the system, waiting plus service time
+
+: expected waiting time in queue
+
+
+2)
+1- sim/theory
+Simulation Results:
+L = 4.808672
+ = 3.979319
+W = 0.480263
+ = 0.397092
+
+Percentage of Theoretical Value:
+1- sim/theory
+
+
+
+
+
+%calculate the mean number waiting
+meanNumWaiting = mean(cellfun(@(q) mean(q.Log.NumWaiting), QSamples))
+
+%calculate the expected waiting time in queue
+
+TimeInQueueSamples = cellfun( ...
+    @(q) cellfun(@(c) c.BeginServiceTime - c.ArrivalTime, q.Served'), ...
+    QSamples, ...
+    UniformOutput=false);
+TimeInQueue = vertcat(TimeInQueueSamples{:});
+meanTimeInQueue = mean(TimeInQueue);
+fprintf("Mean time in queue: %f\n", meanTimeInQueue);
+
+
+
+%q.Served - c.ArrivalTime
+%expectedWaitingTimeQueue = mean(cellfun(@(q) mean(q.Log.)))
+
+customercount = length(TimeInQueue)
+toolong = 0 
+justfine = 0
+for customer = 1:customercount
+    if TimeInQueue(customer) > 5
+        toolong = toolong + 1;
+    else
+        justfine = justfine + 1;
+    end
+end
+fprintf("Num cust waited too long: %f\n", toolong)
+fprintf("Num cust waited just fine: %f\n", justfine)
+proptoolong = toolong/(toolong+justfine)
